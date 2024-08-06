51aafcaaea4bc8d4aef014f4248f49bdf097ee60
Sumit Singh <124269664+Rusty-98@users.noreply.github.com>
2024-08-06 09:58:24 +0530

    done changes

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -7,7 +7,7 @@ const Pagination = ({ totalItems, itemsPerPage, currentPage, onPageChange }) => 
           <button
             key={i}
             onClick={() => onPageChange(i + 1)}
-            className={`px-4 py-2 mx-1 border rounded ${
+            className={`px-3 py-2 mx-1 border rounded ${
               currentPage === i + 1 ? 'bg-blue-600 text-white' : 'bg-gray-700 text-blue-400'
             } border-gray-600`}
           >
